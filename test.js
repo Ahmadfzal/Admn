84ef4188a23e32cba07939e9d4fbe75379cf5cd4
Ahmadfzal <113408830+Ahmadfzal@users.noreply.github.com>
2025-12-02 17:13:24 +0800

    test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const sb = supabase.createClient(
-  "https://YOUR_PROJECT_URL.supabase.co",
-  "YOUR_ANON_KEY"
+  "https://deqtolfjenzmskfiocxl.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRlcXRvbGZqZW56bXNrZmlvY3hsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODYxNzUsImV4cCI6MjA4MDE2MjE3NX0.qJQQlUEKqFrhUAVvqoQWQngm6BCWzv3FuteLOCM4yOg"
 );
 
 document.getElementById("btn").onclick = async () => {
